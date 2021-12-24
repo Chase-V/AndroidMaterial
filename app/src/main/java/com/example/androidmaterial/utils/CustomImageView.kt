6ff212d77ddabe68995f8785fab3306fa3ab50c9
Victor <tashev.victor@gmail.com>
2021-12-24 13:16:07 +0300

    Created project for Android Material

---
+++ app/src/main/java/com/example/androidmaterial/utils/CustomImageView.kt
@@ -0,0 +1,12 @@
+package com.example.androidmaterial.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class CustomImageView @JvmOverloads constructor(context:Context, attrs:AttributeSet? = null, style: Int=0)
+    : AppCompatImageView(context, attrs, style){
+
+
+
+}
